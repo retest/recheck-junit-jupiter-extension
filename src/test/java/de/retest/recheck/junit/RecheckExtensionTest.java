e22f3012abef551c67b561f7131975b5b67bc2b2
Lars Briem <lars.briem@briemla.de>
2019-09-19 19:34:18 +0200

    Test extension for class without fields.

--- src/test/java/de/retest/recheck/junit/RecheckExtensionTest.java
+++ src/test/java/de/retest/recheck/junit/RecheckExtensionTest.java
@@ -16,11 +16,16 @@ public class RecheckExtensionTest {
 	private static final String testName = "testName";
 	private ExtensionContext context;
 	private RecheckDummy recheckDummy;
+	private RecheckExtension extension;
 
 	private static class RecheckDummy {
 
 		private RecheckLifecycle recheck;
 		private Runnable someField;
+	}
+
+	private static class EmptyTest {
+
 	}
 
 	@BeforeEach
@@ -29,36 +34,53 @@ public class RecheckExtensionTest {
 		recheckDummy = new RecheckDummy();
 		recheckDummy.recheck = mock( RecheckLifecycle.class );
 		recheckDummy.someField = mock( Runnable.class );
-		when( context.getRequiredTestInstance() ).thenReturn( recheckDummy );
-		when( context.getRequiredTestClass() ).then( i -> recheckDummy.getClass() );
+		configure( recheckDummy );
 		when( context.getDisplayName() ).thenReturn( testName );
+
+		extension = new RecheckExtension();
+	}
+
+	private void configure( final Object testInstance ) {
+		when( context.getRequiredTestInstance() ).thenReturn( testInstance );
+		when( context.getRequiredTestClass() ).then( i -> testInstance.getClass() );
 	}
 
 	@Test
 	void startsTest() throws Exception {
-		new RecheckExtension().beforeTestExecution( context );
+		extension.beforeTestExecution( context );
 
 		verify( recheckDummy.recheck ).startTest( testName );
 	}
 
 	@Test
 	void capsTest() throws Exception {
-		new RecheckExtension().afterTestExecution( context );
+		extension.afterTestExecution( context );
 
 		verify( recheckDummy.recheck ).capTest();
 	}
 
 	@Test
 	void capsAfterAll() throws Exception {
-		new RecheckExtension().afterAll( context );
+		extension.afterAll( context );
 
 		verify( recheckDummy.recheck ).cap();
 	}
 
 	@Test
 	void callsNothingOnOtherMembers() throws Exception {
-		new RecheckExtension().afterAll( context );
+		extension.beforeTestExecution( context );
+		extension.afterTestExecution( context );
+		extension.afterAll( context );
 
 		verifyZeroInteractions( recheckDummy.someField );
+	}
+
+	@Test
+	void doesNotFailOnEmptyTest() throws Exception {
+		configure( new EmptyTest() );
+
+		extension.beforeTestExecution( context );
+		extension.afterTestExecution( context );
+		extension.afterAll( context );
 	}
 }

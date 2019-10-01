7293a83ea65237a5c7573fcd3d84b71014433a34
Lars Briem <lars.briem@briemla.de>
2019-10-01 13:15:47 +0200

    Add files for travis ci.

---
+++ ci/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+###### Maven ######
+# Compile with JDK 8
+mvn ${MVN_ARGS} clean package -DskipTests
+
+# Test with JDK 11
+wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 13
+
+if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
+    mvn ${MVN_ARGS} org.jacoco:jacoco-maven-plugin:prepare-agent test verify sonar:sonar
+else
+    mvn ${MVN_ARGS} org.jacoco:jacoco-maven-plugin:prepare-agent test verify
+fi
